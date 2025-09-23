d80c01d9be2372501e6f4ea6f7e8898fe9149412
leop <yukaleo43@gmail.com>
2025-09-23 15:22:59 -0300

    Correccion dependecia prisma en horario

--- src/horario/horario.module.ts
+++ src/horario/horario.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HorarioService } from './horario.service';
 import { HorarioController } from './horario.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [HorarioController],
-  providers: [HorarioService],
+  providers: [HorarioService, PrismaService],
 })
 export class HorarioModule {}
