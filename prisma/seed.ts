ea22812148a97b9be6896888aedd278f93f518bd
leop <yukaleo43@gmail.com>
2026-07-08 11:07:56 -0300

    Estado agregado acorde al diagrama

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,6 +14,7 @@ async function main() {
     data: [
       { ambito: 'PEDIDO', nombre: 'CREADO' },
       { ambito: 'PEDIDO', nombre: 'ENPREPARACION' },
+      { ambito: 'PEDIDO', nombre: 'PUBLICADO' },
       { ambito: 'PEDIDO', nombre: 'ASIGNADO' },
       { ambito: 'PEDIDO', nombre: 'ENRUTA' },
       { ambito: 'PEDIDO', nombre: 'ENTREGADO' },
